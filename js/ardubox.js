48f3ba8b80641c75c8d5dfbe28d3fe4dab331174
HugoCuba <hscuba@gmail.com>
2017-11-17 18:13:02 -0200

    Esboco do layout feito

--- js/ardubox.js
+++ js/ardubox.js
@@ -1,8 +1,8 @@
 $( document ).ready(function() {
     $('.button-collapse').sideNav({
-          //menuWidth: 300, // Default is 240
-          edge: 'left', // Choose the horizontal origin
-          closeOnClick: true // Closes side-nav on <a> clicks, useful for Angular/Meteor
+          menuWidth: 300,
+          edge: 'left',
+          closeOnClick: true
         }
       );
     
