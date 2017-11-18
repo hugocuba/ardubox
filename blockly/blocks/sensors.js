7c1f402ddf7be4676b7ea151011034ef37629ff0
hugocuba <HgsllscB31038816>
2017-11-18 19:41:44 -0200

    Inicio do desenvolvimento de blocos especificos

---
+++ blockly/blocks/sensors.js
@@ -0,0 +1,13 @@
+Blockly.Blocks['sensor_led13'] = {
+    init: function() {
+        this.appendDummyInput()
+            .appendField(new Blockly.FieldImage("https://i.imgur.com/uDpddAT.jpg", 120, 70, ""))
+            .appendField("Led Integrado")
+            .appendField(new Blockly.FieldDropdown([["Ligado","HIGH"], ["Desligado","LOW"]]), "estado");
+        this.setPreviousStatement(true, null);
+        this.setNextStatement(true, null);
+        this.setColour(230);
+        this.setTooltip("Pino 13");
+        this.setHelpUrl("");
+    }
+};
